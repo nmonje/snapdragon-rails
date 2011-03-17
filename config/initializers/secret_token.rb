7cb68ede6e0ef239c472d0e0f7d2c66b91109f2a
nmonje <nicholas.monje@gmail.com>
2011-03-17 17:07:39 -0400

    So fresh and clean.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnapdragonRails::Application.config.secret_token = '48c582d9af779d3c1c52c74eca56fa31f66825f41fb8a25b71852cf0219bfa83a178fc27162bdea9247dd152f457437e23316061cabb06bcd54f2fecb95fea50'
